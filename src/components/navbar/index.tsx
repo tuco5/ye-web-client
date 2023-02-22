a1fcfa5c0f9a451f31298d783ebb24d72f158e6a
Tudor <tudor.comparan@gmail.com>
2023-02-21 18:40:52 -0600

    navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,19 +1,34 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import styles from './navbar.module.scss';
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
-        <Link href="/">
-          <Image className={styles.logo} src="/logo.png" width={100} height={100} alt="logo" />
+        <Link href="/" className={styles.logo}>
+          <Image src="/logo-min.png" width={120} height={60} alt="logo" />
+          <p className={styles.logoText}>
+            Yoga <span className={styles.plus}>+</span> Escalada
+          </p>
         </Link>
 
         <div className={styles.menu}>
-          <Link href="/about-us">¿Quienes somos?</Link>
-          <Link href="/activities">Actividades</Link>
-          <Link href="/testimonies">Testimonios</Link>
+          <Link href="/about-us" className={`${styles.link} ${pathname === '/about-us' && styles.active}`}>
+            ¿Quienes somos?
+          </Link>
+          <Link href="/activities" className={`${styles.link} ${pathname === '/activities' && styles.active}`}>
+            Actividades
+          </Link>
+          <Link href="/testimonies" className={`${styles.link} ${pathname === '/testimonies' && styles.active}`}>
+            Testimonios
+          </Link>
         </div>
       </div>
     </nav>
