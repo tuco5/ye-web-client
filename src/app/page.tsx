38d33a3c723531f7191b4f087314b8fc8a9ab578
Tudor <tudor.comparan@gmail.com>
2023-02-23 08:16:36 -0600

    home page hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
+import Link from 'next/link';
 import styles from './page.module.scss';
 
 export default function Home() {
-  return <main className={styles.main}>Home</main>;
+  return (
+    <>
+      <main className={styles.main}>
+        <div className={styles.hero__img}>&nbsp;</div>
+
+        <div className={styles.hero__banner}>
+          <div className={styles.hero__cta}>
+            <Link href="/activities" className={styles.btn__main}>
+              Informes
+            </Link>
+          </div>
+          <h1 className={styles.title}>
+            Less Talk<span className={styles.span}>.</span>
+          </h1>
+          <h2 className={styles.subtitle}>
+            More Chalk<span className={styles.span}>.</span>
+          </h2>
+        </div>
+      </main>
+      <section className={styles.section__about}>
+        <p>
+          El rocódromo más grande en la zona de Zapopan Jalisco<span className={styles.span}>.</span>
+        </p>
+      </section>
+      <section className={styles.section__info}></section>
+    </>
+  );
 }
