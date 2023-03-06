0c6f4bfb691accfd685d76f6f9b60928cb513d1a
Tudor Comaparan <tudor.comparan@gmail.com>
2023-03-06 08:39:47 -0600

    Scroll (#4)
    * fix scroll on yoga and climbing link
    
    * replace Link for a tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function HomePage() {
                 <Button href="/prices/#climbing" variant="text" type="link">
                   Escalada &rarr;
                 </Button>
-                <Button href="/prices/#yoga" variant="text" type="link" scroll={false}>
+                <Button href="/prices#yoga" variant="text" type="link">
                   Yoga &rarr;
                 </Button>
               </ul>
