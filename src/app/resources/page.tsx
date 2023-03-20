957dac2cd96bab9e0cc74afc939ee6ea92c53fae
Tudor Comaparan <tudor.comparan@gmail.com>
2023-03-19 18:56:51 -0600

    Resources guides
    * Images optimization
    
    * modify import
    
    * navbar dynamic on scroll
    
    * carousel speed updated
    
    * animation to hero image
    
    * big desktop layout
    
    * mobile menu triggering all the stuff
    
    * mobile menu done
    
    * fif of mobile menu bug
    
    * section team
    
    * fix prices background
    
    * Pricing page done
    
    * fixing on home page link
    
    * prices subtitle update
    
    * update big desktop bg
    
    * demo resources page

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,12 +1,20 @@
+import { Title } from '@/components/atoms';
+import { ResourcesCard } from '@/components/molecules';
 import styles from './page.module.scss';
-import ClockIcon from '../../../public/icons/clock.svg';
+
 export default function ResourcesPage() {
   return (
     <section className={styles.section}>
-      <h2 className="coming_soon">
-        Coming soon <span className="green__500">.</span>
-      </h2>
-      <ClockIcon className="coming_soon__icon" />
+      <Title variant="rainbow-blue" className={styles.section__title}>
+        encuentra la veta
+      </Title>
+
+      <div className={styles.section__grid}>
+        <ResourcesCard image="/images/el-diente.jpg" name="El Diente" resourcePath="/guias/el-diente.pdf" locationHref="https://goo.gl/maps/4iMciBzXXUeM4Uky6" />
+        <ResourcesCard image="/images/el-diente.jpg" name="El Diente" resourcePath="/guias/el-diente.pdf" locationHref="https://goo.gl/maps/4iMciBzXXUeM4Uky6" />
+        <ResourcesCard image="/images/el-diente.jpg" name="El Diente" resourcePath="/guias/el-diente.pdf" locationHref="https://goo.gl/maps/4iMciBzXXUeM4Uky6" />
+        <ResourcesCard image="/images/el-diente.jpg" name="El Diente" resourcePath="/guias/el-diente.pdf" locationHref="https://goo.gl/maps/4iMciBzXXUeM4Uky6" />
+      </div>
     </section>
   );
 }
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,3 +1,4 @@
 export { FeatureBox } from './feature-box';
 export { BackgroundVideo } from './background-video';
 export { ReviewCard } from './review-card';
+export { ResourcesCard } from './resources-card';
