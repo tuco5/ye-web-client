faeb9015a4e3ce42900e44bf34a5f1a1157cb1c1
Tudor Comaparan <tudor.comparan@gmail.com>
2023-03-19 19:21:19 -0600

    Fix big desktop bg (#10)
    * Images optimization
    
    * modify import
    
    * navbar dynamic on scroll
    
    * carousel speed updated
    
    * animation to hero image
    
    * big desktop layout
    
    * mobile menu triggering all the stuff
    
    * mobile menu done
    
    * fif of mobile menu bug
    
    * section team
    
    * fix prices background
    
    * Pricing page done
    
    * fixing on home page link
    
    * prices subtitle update
    
    * update big desktop bg
    
    * demo resources page
    
    * resources guides done
    
    * big desktop bg update

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,19 +1,20 @@
-import { Title } from '@/components/atoms';
+import { Subtitle, Title } from '@/components/atoms';
 import { ResourcesCard } from '@/components/molecules';
 import styles from './page.module.scss';
 
 export default function ResourcesPage() {
   return (
-    <section className={styles.section}>
+    <section className={styles.section} title="Guias">
       <Title variant="rainbow-blue" className={styles.section__title}>
-        encuentra la veta
+        Guías
       </Title>
+      <Subtitle className={styles.section__subtitle}>Encuentra la veta</Subtitle>
 
       <div className={styles.section__grid}>
         <ResourcesCard image="/images/el-diente.jpg" name="El Diente" resourcePath="/guias/el-diente.pdf" locationHref="https://goo.gl/maps/4iMciBzXXUeM4Uky6" />
-        <ResourcesCard image="/images/el-diente.jpg" name="El Diente" resourcePath="/guias/el-diente.pdf" locationHref="https://goo.gl/maps/4iMciBzXXUeM4Uky6" />
-        <ResourcesCard image="/images/el-diente.jpg" name="El Diente" resourcePath="/guias/el-diente.pdf" locationHref="https://goo.gl/maps/4iMciBzXXUeM4Uky6" />
-        <ResourcesCard image="/images/el-diente.jpg" name="El Diente" resourcePath="/guias/el-diente.pdf" locationHref="https://goo.gl/maps/4iMciBzXXUeM4Uky6" />
+        <ResourcesCard image="/images/el-cuajo.jpeg" name="El Cuajo" resourcePath="/guias/el-cuajo.pdf" locationHref="https://goo.gl/maps/ccoAxugsruuWqwsHA" />
+        <ResourcesCard image="/images/ixcatan.jpg" name="Ixcatan" resourcePath="/guias/ixcatan.pdf" locationHref="https://goo.gl/maps/mMrWfm1ViGYy9TgXA" />
+        <ResourcesCard image="/images/bernal.jpg" name="Bernal" resourcePath="/guias/bernal.pdf" locationHref="https://goo.gl/maps/Mpp4QL97fzZgBui89" />
       </div>
     </section>
   );
